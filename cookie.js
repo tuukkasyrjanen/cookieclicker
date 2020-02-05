bf0d473b1b76368466194d5805a8a80a9c794946
Tuukka <tuukka.syrjanen@aalto.fi>
2020-02-05 20:56:27 +0200

    Jaoston kokeiluun

--- cookie.js
+++ cookie.js
@@ -1,25 +1,32 @@
 //initial number of cookies
 var num = 0;
-
-
-
-
+var size = "";
+var px = "px";
 var cookie = document.getElementById("cookie");
+var audio = new Audio('morseclue.wav');
 
 function cookieClick() {
-    num += 1;
-
+    num += 100;
     var numbers = document.getElementById("numbers");
-
     //upgrade level for printing
-
-
-
-
     numbers.innerHTML = num;
+    if (num > 500){audio.play();}
+    var keksi = 100+ (num/5)
+    size = String(keksi).concat("px");
 
-    if (num > 99){
-    var pic = document.getElementById("picture").src="clue.png";
-    picture.innerHTML = pic;
+    document.getElementById('cookie').style.width= size ;
+
+    if (num >= 100 && num <= 150){
+      numbers.innerHTML = "Waiting for clue?";
+    }
+
+    if (num >= 200 && num <= 250){
+      numbers.innerHTML = "Almost there!";
+    }
+
+    if (num > 500){
+      numbers.innerHTML = "What's that sound?";
+      cookie.innerHTML = document.getElementById("cookie").src="clue.png";
+
     }
 }
