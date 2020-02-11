b27f8762c3939d40e026083b5cf6117d96fc803a
Tuukka <tuukka.syrjanen@aalto.fi>
2020-02-11 14:45:08 +0200

    Ääni loopille ja wikipedia

--- cookie.js
+++ cookie.js
@@ -3,7 +3,9 @@ var num = 0;
 var size = "";
 var px = "px";
 var cookie = document.getElementById("cookie");
+var morse = document.getElementById("morse");
 var audio = new Audio('morseclue.wav');
+audio.loop = true;
 
 function cookieClick() {
     num += 1;
@@ -16,17 +18,39 @@ function cookieClick() {
 
     document.getElementById('cookie').style.width= size ;
 
-    if (num >= 100 && num <= 150){
+    if (num >= 50 && num < 100){
+      morse.innerHTML = document.getElementById("morse").src = "morse1.png";
+
+    }
+    if (num >= 100 && num < 150){
       numbers.innerHTML = "Waiting for clue?";
+      morse.innerHTML = document.getElementById("morse").src = "morse2.png";
     }
 
-    if (num >= 200 && num <= 250){
+    if (num >= 150 && num < 200){
+      morse.innerHTML = document.getElementById("morse").src = "morse3.png";
+    }
+    if (num >= 200 && num < 250){
       numbers.innerHTML = "Almost there!";
+      morse.innerHTML = document.getElementById("morse").src = "morse4.png";
     }
 
-    if (num > 500){
+    if (num >= 250 && num < 350){
+      morse.innerHTML = document.getElementById("morse").src = "morse5.png";
+    }
+
+    if (num >= 350 && num < 400){
+      morse.innerHTML = document.getElementById("morse").src = "morse6.png";
+    }
+
+    if (num >= 400 && num < 450){
+      morse.innerHTML = document.getElementById("morse").src = "morse7.png";
+    }
+
+
+    if (num >= 500){
       numbers.innerHTML = "What's that sound?";
       cookie.innerHTML = document.getElementById("cookie").src="clue.png";
-
+      morse.innerHTML = document.getElementById("morse").src = "morsefinal.png";
     }
 }
