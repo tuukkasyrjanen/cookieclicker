b99e98bb82dc16c58248982c9885d539111202e5
Tuukka <tuukka.syrjanen@aalto.fi>
2020-03-10 17:38:09 +0200

    Morseblur

--- cookie.js
+++ cookie.js
@@ -8,7 +8,7 @@ var audio = new Audio('morseclue.wav');
 audio.loop = true;
 
 function cookieClick() {
-    num += 1;
+    num += 20;
     var numbers = document.getElementById("numbers");
     //upgrade level for printing
     numbers.innerHTML = num;
@@ -19,38 +19,38 @@ function cookieClick() {
     document.getElementById('cookie').style.width= size ;
 
     if (num >= 50 && num < 100){
-      morse.innerHTML = document.getElementById("morse").src = "morse1.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur1.png";
 
     }
     if (num >= 100 && num < 150){
       numbers.innerHTML = "Waiting for clue?";
-      morse.innerHTML = document.getElementById("morse").src = "morse2.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur2.png";
     }
 
     if (num >= 150 && num < 200){
-      morse.innerHTML = document.getElementById("morse").src = "morse3.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur3.png";
     }
     if (num >= 200 && num < 250){
       numbers.innerHTML = "Almost there!";
-      morse.innerHTML = document.getElementById("morse").src = "morse4.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur4.png";
     }
 
     if (num >= 250 && num < 350){
-      morse.innerHTML = document.getElementById("morse").src = "morse5.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur5.png";
     }
 
     if (num >= 350 && num < 400){
-      morse.innerHTML = document.getElementById("morse").src = "morse6.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur5.png";
     }
 
     if (num >= 400 && num < 450){
-      morse.innerHTML = document.getElementById("morse").src = "morse7.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur5.png";
     }
 
 
     if (num >= 500){
       numbers.innerHTML = "What's that sound?";
       cookie.innerHTML = document.getElementById("cookie").src="clue.png";
-      morse.innerHTML = document.getElementById("morse").src = "morsefinal.png";
+      morse.innerHTML = document.getElementById("morse").src = "morsewideblur5.png";
     }
 }
